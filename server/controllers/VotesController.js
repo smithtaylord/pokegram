e61a9dd22cd283bbf362344c1054aa8ecc026c00
Jaden Hawkes <jadenhawkes@gmail.com>
2023-02-17 14:46:01 -0700

    Final touches

--- server/controllers/VotesController.js
+++ server/controllers/VotesController.js
@@ -1,8 +1,10 @@
 import BaseController from "../utils/BaseController";
+import { Auth0Provider } from "@bcwdev/auth0provider";
 
 export class VotesController extends BaseController {
 constructor(){
   super('api/votes')
   this.router
+  .use(Auth0Provider.getAuthorizedUserInfo)
 }
 }
