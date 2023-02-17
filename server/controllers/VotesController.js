d67d3c62c96c08f64872051ff4bfa2c869138255
Payton Boone <paytonboone30@gmail.com>
2023-02-17 15:33:24 -0700

    setting up votes

--- server/controllers/VotesController.js
+++ server/controllers/VotesController.js
@@ -1,10 +1,24 @@
 import BaseController from "../utils/BaseController";
 import { Auth0Provider } from "@bcwdev/auth0provider";
+import { votesService } from "../services/VotesService.js";
 
 export class VotesController extends BaseController {
 constructor(){
   super('api/votes')
   this.router
   .use(Auth0Provider.getAuthorizedUserInfo)
+  .post('', this.increasePokemonVote)
+}
+
+
+async increasePokemonVote(req, res, next) {
+  try {
+    const user = req.userInfo
+    req.body.trainerId = user.id 
+    const voter = await votesService.increasePokemonVote(req.body)
+    return res.send(voter)
+  } catch (error) {
+    next(error)
+  }
 }
 }
--- server/services/VotesService.js
+++ server/services/VotesService.js
@@ -1,5 +1,10 @@
-class VotesService{
+import { dbContext } from "../db/DbContext.js"
 
+class VotesService{
+    async increasePokemonVote(pokemonData) {
+        const voter = await dbContext.Votes.create(pokemonData)
+        return voter
+    }
 }
 
 export const votesService = new VotesService()
