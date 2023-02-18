2735bc74047dc2df9ff0ec41057c95913e18e875
Taylor Smith <smith.taylord@gmail.com>
2023-02-17 22:44:55 -0700

    virtual added for trainer name

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -3,28 +3,28 @@ import { dbContext } from "../db/DbContext"
 import { BadRequest } from "../utils/Errors"
 
 class CommentsService {
- 
+
 
    async getAllCommentsByPokemonId(pokemonId) {
 
-       const comments = await dbContext.Comments.find({pokemonId})
-       if (!comments) {
-        throw new BadRequest("bad pokemon ID")
-       }
-       return comments
+      const comments = await dbContext.Comments.find({ pokemonId }).populate('trainer')
+      if (!comments) {
+         throw new BadRequest("bad pokemon ID")
+      }
+      return comments
    }
 
-async createComment(newComment) {
-//    const pokemon = await dbContext.Pokemons.findById(newComment.pokemonId) 
-//    if (!pokemon) {
-//     throw new BadRequest('there is no pokemon with this ID')
-//    }
+   async createComment(newComment) {
+      //    const pokemon = await dbContext.Pokemons.findById(newComment.pokemonId) 
+      //    if (!pokemon) {
+      //     throw new BadRequest('there is no pokemon with this ID')
+      //    }
 
-   const comment = await dbContext.Comments.create(newComment)
-   return comment
+      const comment = await dbContext.Comments.create(newComment)
+      return comment
 
 
-}
+   }
 
 }
 
